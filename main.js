eaa9e8650406dc02b1b11f2a6dc524cf2b97be2c
Rajdeep Anand <112320985+qwertycod@users.noreply.github.com>
2023-08-04 22:38:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -34,3 +34,43 @@ saveButton.addEventListener('click', async () => {
   }, 100);
 });
 })
+
+//If you want to pass or save multiple value in the chrome.storage.local.set() method and get all those values.
+
+//It can be done by :
+
+//Notice how are passing the parameters in sequence;  [currentId, submitData, userName]
+
+//and using them in the function in same sequence async (selectedValue, submitData, userName)
+  
+  // saveButton.addEventListener('click', async () => {
+  //   var currentId = document.getElementById("currentId").value;
+  //   var submitData = document.getElementById("setGCode").checked;
+  //   var userName = document.getElementById("userName").value;
+    
+  //   chrome.tabs.query({active: true, currentWindow: true}).then(([tab]) => {
+  //     chrome.scripting.executeScript(
+  //       {
+  //         target: {tabId: tab.id},
+  //          function: async (selectedValue, submitData, userName) => {
+  //           try {
+  //             console.log('selectedValue', selectedValue + ' submitData ' + submitData +  ' userName ' + userName  );    
+  
+  //             chrome.storage.local.set({ "currentNumer": selectedValue, "submitData" : submitData, "userName" : userName  }).then(() => {
+  //               console.log("------- All data is set -------");
+  //             });       
+  //           }
+  //           catch (e) {
+  //               console.log(e)
+  //           }
+  //       },
+  //       args: [currentId, submitData, userName]
+  //     });
+  // }).then(function(){
+  //   setTimeout(() => {
+  //     console.log('selectedValue :- ', currentId);
+  //     document.getElementById("currentIdLabel").textContent  = currentId;
+  //     saveButton.style.background = "LightGreen";
+  //   }, 100);
+  // });
+  // })
